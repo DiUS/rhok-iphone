36291af1183baa518e29c41d3ce650b6868d1509
Erik Danielsen <edanielsen@gmail.com>
2012-06-02 19:47:57 +1000

    First test of parse.com call

--- Resources/controller/IssueController.js
+++ Resources/controller/IssueController.js
@@ -2,6 +2,12 @@ rhok.controller.issueController = function() {
 
 	createIssue = function() {
 		Ti.API.info('I can has logging');
+		rhok.network.xhr(
+			'https://api.parse.com/1/classes/Issue', 
+			function(response) {
+				Ti.API.info(response);
+			}
+		);
 	}
 	
 	return {
--- Resources/controller/controllers.js
+++ Resources/controller/controllers.js
@@ -1,9 +1,6 @@
-rhok.network = (function (){ 
-	var me = {};
-
-	me.xhr = function(url, responseHandler, options) {
-    
-	    var method = "GET";
+rhok.network = function (){ 
+	xhr = function(url, responseHandler, options) {
+ 	    var method = "GET";
 	    var contentType = "JSON";
 	    var errorCallback = null;
 	    var postParams = null;
@@ -25,26 +22,29 @@ rhok.network = (function (){
 	        postParams = options.postParams;
 	      }
 	    }
-	    
+
 	    var xhr = Ti.Network.createHTTPClient({
 	      onload : function(e) {
 	        if(this.status == 200) {
+	        	Ti.API.info(this.responseText);
 	          var parsedResponse = JSON.parse(this.responseText);
-	          Ti.API.debug(parsedResponse);
 	          responseHandler(parsedResponse);
 	        } else {
-	          _wrappedErrorCallback(errorCallback).displayServerError(this.status, this.responseText);
+	        	Ti.API.info(this.status);
+	          //Do something here
 	        }
 	      },
-	      onerror : _wrappedErrorCallback(errorCallback).registerFailedCallback,
+	      onerror : function(e) { Ti.API.error(e.error); },
 	      timeout : 10000
 	    });
 	
-	    xhr.open(method,  _hashedUrl(me.piggybackUserId(url)));
-	    
+	    xhr.open(method,  url);
+
 	    if (contentType == "JSON") {
 	      xhr.setRequestHeader("Content-Type", "application/json");
 	      xhr.setRequestHeader("Accept", "application/json");
+	      xhr.setRequestHeader("X-Parse-Application-Id", "lGZqd3iRqUPZCEjwmWDp4EKWV1dd0xofryNY4TuB");
+	      xhr.setRequestHeader("X-Parse-REST-API-Key", "QwfvNEkNjAOEXeydTbwkCdaI2RdAQoQKfUlImEVH");
 	    }
 	    
 	    if (postParams === null) {
@@ -53,6 +53,10 @@ rhok.network = (function (){
 	      xhr.send(postParams);
 	    }
 	};
-	return me;
-})();
+	return {
+		xhr : function(url, responseHandler, options) {
+			xhr(url, responseHandler, options);
+		}
+	};
+}();
 Ti.include('IssueController.js');
