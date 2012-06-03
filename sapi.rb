eb20f699974b6d312a194b9d00f9bba2ccd9e221
Julian Kelabora <jkelabora@dius.com.au>
2012-06-03 10:56:37 +1000

    sinatra-fy the sapi interface

--- sapi.rb
+++ sapi.rb
@@ -1,25 +1,20 @@
+require 'sinatra'
 require 'net/http'
 require 'json'
 
 endpoint = "http://api.sensis.com.au/ob-20110511/test/search" 
-
 key = "jx3fpbttj8pf68a6ywcnaqpz"
-query = "fencing contractors"
 
-#woodend!!
-location = "-37.3568115234,144.5272674561"
+get '/sapi' do
 
-url = "#{endpoint}?key=#{key}&query=#{URI.encode(query)}&location=#{URI.encode(location)}"
+  # woodend!! http://0.0.0.0:4567/sapi?category=fencing&location=-37.3568115234,144.5272674561
+  url = "#{endpoint}?key=#{key}&query=#{URI.encode(params[:category])}&location=#{URI.encode(params[:location])}"
 
-response = Net::HTTP.get_response(URI.parse(url))
+  response = Net::HTTP.get_response(URI.parse(url))
+  result = JSON.parse(response.body)
+  filtered = result["results"].select{ |a| a["primaryContacts"].any?{ |h| h["type"] == "PHONE" } }
+  name_nums = filtered.collect{ |result| { result["name"] => result["primaryContacts"].select{ |h| h["type"] == "PHONE" }.first["value"] } }
 
-data = response.body
+  name_nums.sort_by{ |h| h.keys.first }.to_json
 
-result = JSON.parse(data)
-
-filtered = result["results"].select{ |a| a["primaryContacts"].any?{ |h| h["type"] == "PHONE" } }
-
-name_nums = filtered.collect{ |result| { result["name"] => result["primaryContacts"].select{ |h| h["type"] == "PHONE" }.first["value"] } }
-
-puts name_nums.sort_by{ |h| h.keys.first }
-
+end
